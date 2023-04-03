1a16b31ca23b6cba6df3b6f699b005e39490cbbb
yoshikouki <yoshikouki@gmail.com>
2023-04-03 11:03:43 +0900

    Build next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/utils/api";
-
+import { M_PLUS_1 } from "next/font/google";
 import "~/styles/globals.css";
+
+const mPlus1 = M_PLUS_1({
+  weight: ["400", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["system-ui", "arial"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +18,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={mPlus1.className}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
