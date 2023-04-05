e8f18cd7837260053068f5257cdda52e019ef9e4
yoshikouki <yoshikouki@gmail.com>
2023-04-05 23:29:09 +0900

    Update for test factory

--- src/server/repositories/character.repository.ts
+++ src/server/repositories/character.repository.ts
@@ -2,32 +2,32 @@ import { type BattleCharacter, type Character } from "~/types/character.type";
 
 import { faker } from "@faker-js/faker";
 
-export const generateCharacter = (): Character => {
+export const generateCharacter = (attr?: Partial<Character>): Character => {
   return {
-    id: faker.datatype.uuid(),
-    name: faker.internet.userName(),
-    level: faker.datatype.number({ min: 1000, max: 5000 }),
-    hitPoint: faker.datatype.number({ min: 10000, max: 50000 }),
-    attack: faker.datatype.number({ min: 3000, max: 8000 }),
-    defense: faker.datatype.number({ min: 1000, max: 5000 }),
-    speed: faker.datatype.number({ min: 1000, max: 5000 }),
+    id: attr?.id || faker.datatype.uuid(),
+    name: attr?.name || faker.internet.userName(),
+    level: attr?.level || faker.datatype.number({ min: 1000, max: 5000 }),
+    hitPoint: attr?.hitPoint || faker.datatype.number({ min: 10000, max: 50000 }),
+    attack: attr?.attack || faker.datatype.number({ min: 3000, max: 8000 }),
+    defense: attr?.defense || faker.datatype.number({ min: 1000, max: 5000 }),
+    speed: attr?.speed || faker.datatype.number({ min: 1000, max: 5000 }),
   };
 };
 
-export const generateBattleCharacter = (): BattleCharacter => {
-  const character = generateCharacter();
+export const generateBattleCharacter = (attr?: Partial<BattleCharacter> & { character?: Character }): BattleCharacter => {
+  const character = attr?.character || generateCharacter();
   return {
-    id: character.id,
-    name: character.name,
-    level: character.level,
-    actionPoints: 0,
-    initialHitPoint: character.hitPoint,
-    currentHitPoint: character.hitPoint,
-    initialAttack: character.attack,
-    currentAttack: character.attack,
-    initialDefense: character.defense,
-    currentDefense: character.defense,
-    initialSpeed: character.speed,
-    currentSpeed: character.speed,
+    id: attr?.id || character.id,
+    name: attr?.name || character.name,
+    level: attr?.level || character.level,
+    actionPoints: attr?.actionPoints || 0,
+    initialHitPoint: attr?.initialHitPoint || character.hitPoint,
+    currentHitPoint: attr?.currentHitPoint || character.hitPoint,
+    initialAttack: attr?.initialAttack || character.attack,
+    currentAttack: attr?.currentAttack || character.attack,
+    initialDefense: attr?.initialDefense || character.defense,
+    currentDefense: attr?.currentDefense || character.defense,
+    initialSpeed: attr?.initialSpeed || character.speed,
+    currentSpeed: attr?.currentSpeed || character.speed,
   };
 };
