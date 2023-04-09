b7acba5ccfc312a94cedd755ac14e2ea08dbd7f1
yoshikouki <yoshikouki@gmail.com>
2023-04-09 11:56:28 +0900

    Separate battle logs component

--- src/pages/battle.tsx
+++ src/pages/battle.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { type NextPage } from "next";
 import { ScrollToBottomButton } from "~/components/ScrollToBottomButton";
+import { BattleLogs } from "~/components/battle/BattleLogs";
 import { BattleStatus } from "~/components/battle/BattleStatus";
 import DefaultLayout from "~/components/layout/DefaultLayout";
 import { api } from "~/utils/api";
@@ -19,68 +20,7 @@ const BattlePage: NextPage = () => {
             <BattleStatus player={result.player} enemy={result.enemy} />
           </div>
 
-          <div className="flex flex-col space-y-12">
-            {result?.battleLogs.map((log) => (
-              <div
-                key={log.actionsNumber}
-                className={classNames("flex items-center space-x-2 p-2", {
-                  "justify-start": log.isPlayerAttack,
-                  "justify-end": !log.isPlayerAttack,
-                })}
-              >
-                <div
-                  className={classNames("flex-1", {
-                    "text-left": log.isPlayerAttack,
-                    "text-right": !log.isPlayerAttack,
-                  })}
-                >
-                  <div className="mb-3 w-full text-center">
-                    <span className="badge-ghost badge">
-                      {log.actionsNumber}
-                    </span>
-                  </div>
-                  <div className="mb-2">
-                    <span className="mr-1 font-bold">{log.attacker.name}</span>
-                    の攻撃！
-                  </div>
-                  <div className="mb-4">
-                    <span className="mr-1 text-4xl font-extrabold">
-                      {log.damage}
-                    </span>
-                    のダメージ！
-                  </div>
-                  <div
-                    className={classNames(
-                      "overflow-hiddenflex relative mb-2 h-4 w-full rounded-lg bg-base-200",
-                      "tooltip tooltip-bottom",
-                      {
-                        "tooltip-accent": log.isPlayerAttack,
-                        "tooltip-primary": !log.isPlayerAttack,
-                      }
-                    )}
-                    data-tip={`${log.defender.currentHitPoint} / ${log.defender.initialHitPoint}`}
-                  >
-                    <div
-                      className={classNames([
-                        "absolute h-4 rounded-lg",
-                        {
-                          "right-0 bg-accent": log.isPlayerAttack,
-                          "justify-start bg-primary": !log.isPlayerAttack,
-                        },
-                      ])}
-                      style={{
-                        width: `${
-                          (log.defender.currentHitPoint /
-                            log.defender.initialHitPoint) *
-                          100
-                        }%`,
-                      }}
-                    />
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+          {result.battleLogs && <BattleLogs battleLogs={result.battleLogs} />}
 
           <div
             className={classNames(
